a3314169367a60ea67a6b0d20e94f685c049b20b
Varshini S <varshinigowdasv@gmail.com>
2026-01-31 09:15:49 +0530

    Delete code1.java

--- code1.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class code1{
-    public static void main(String[] args){
-        int i=10;int j=20;
-        System.out.println(i+j);
-        System.out.println(i-j);
-    }
-}
