f85c86d197f8208c9f49045f50a035b69fe662b9
Varshini S <varshinigowdasv@gmail.com>
2026-01-31 09:14:23 +0530

    Create code1.java

---
+++ Day3/code1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class code1{
+    public static void main(String[] args){
+        int i=10;int j=20;
+        System.out.println(i+j);
+        System.out.println(i-j);
+    }
+}
